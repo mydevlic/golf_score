f14e702bb0aefdc9147634a8a19229bf566d7943
mydevlic <77262552+mydevlic@users.noreply.github.com>
2023-10-19 14:36:29 +0900

    Add files via upload

--- golf_score_app/views.py
+++ golf_score_app/views.py
@@ -1,5 +1,10 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, Azure Django App!")
+    #return HttpResponse("Hello, Azure Django App!")
+    return render(request, 'page/index.html')
+
+def capture(request):
+    return render(request, 'page/capture_photo.html')
